5424cf6e0b8db7473c20f01b8bc66eaccb2700ce
Norbert Melzer <timmelzer@googlemail.com>
2013-01-21 19:22:18 +0100

    Loading 64bit libtcod

--- lib/rubylike.rb
+++ lib/rubylike.rb
@@ -8,7 +8,7 @@ module Rubylike
 		extend FFI::Library
 		def self.extended(base)
 			base.ffi_lib ['lib/ext/libSDL.so']
-			base.ffi_lib ['lib/ext/libtcod']
+			base.ffi_lib ['lib/ext/libtcod.so']
 		end
 	end
 
